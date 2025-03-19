e7ed6e02c6d176cc7d2c86e6abac58dcc4f08e68
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-03-19 12:32:46 +0100

    update UserService for registration, login, get user(by id, all, current), password encoding in update

--- StoreApp/src/main/java/org/ecommerce/storeapp/service/UserService.java
+++ StoreApp/src/main/java/org/ecommerce/storeapp/service/UserService.java
@@ -3,6 +3,14 @@ package org.ecommerce.storeapp.service;
 import org.ecommerce.storeapp.model.Role;
 import org.ecommerce.storeapp.model.User;
 import org.ecommerce.storeapp.repository.UserRepository;
+import org.ecommerce.storeapp.security.JwtTokenProvider;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +19,15 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, AuthenticationManager authenticationManager, @Lazy JwtTokenProvider jwtTokenProvider, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> getAllUsers() {
@@ -24,20 +39,51 @@ public class UserService {
     }
 
 
-    public User createUser(String firstName, String lastName, String email, String password) {
+    public User registerUser(String firstName, String lastName, String email, String password) {
         User user = new User();
+
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setEmail(email);
-        user.setPassword(password); //TODO password encoding
+        user.setPassword(passwordEncoder.encode(password)); //TODO password encoding
         user.setRole(Role.USER);
         return userRepository.save(user);
     }
 
-    public User updateUser(int id,
-                           String firstName, String lastName, String email, String password, String role) {
+    public String loginUser(String email, String password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        password
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return jwtTokenProvider.generateToken(user.getEmail(), List.of(Role.USER.name()));
 
-        return userRepository.findById(id).map(user -> {
+    }
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new IllegalArgumentException("No authenticated user found.");
+        }
+        Object principal = authentication.getPrincipal();
+        String email;
+        if (principal instanceof UserDetails) {
+            email = ((UserDetails) principal).getUsername();
+        } else if (principal instanceof String) {
+            email = (String) principal;
+        } else {
+            throw new IllegalArgumentException("Unexpected authentication principal.");
+        }
+        return userRepository.findByEmail(email)
+                .orElseThrow(RuntimeException::new);
+    }
+
+    public User updateUser(String firstName, String lastName, String email, String password, String role) {
+        User user = getCurrentUser();
             if (firstName != null && !firstName.isBlank()) {
                 user.setFirstName(firstName);
             }
@@ -48,7 +94,7 @@ public class UserService {
                 user.setEmail(email);
             }
             if (password != null && !password.isBlank()) {
-                user.setPassword(password); // TODO password encoding
+                user.setPassword(passwordEncoder.encode(password));
             }
             if (role != null) {
                 try {
@@ -58,7 +104,6 @@ public class UserService {
                 }
             }
             return userRepository.save(user);
-        }).orElseThrow(() -> new RuntimeException("User not found"));
     }
 
 
